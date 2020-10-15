fe08007a10309844781ff62331f3728cbc517dde
Maarten Fremouw <maarten@fremouw.nl>
2020-10-15 21:29:59 +0200

    add extern "c" keyword.

--- include/button.h
+++ include/button.h
@@ -1,6 +1,10 @@
 #include "freertos/queue.h"
 #include "driver/gpio.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #define PIN_BIT(x) (1ULL<<x)
 
 #define BUTTON_DOWN (1)
@@ -15,3 +19,7 @@ typedef struct {
 
 QueueHandle_t * button_init(unsigned long long pin_select);
 QueueHandle_t * pulled_button_init(unsigned long long pin_select, gpio_pull_mode_t pull_mode);
+
+#ifdef __cplusplus
+}
+#endif
